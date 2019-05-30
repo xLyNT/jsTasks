502b7fa6227d720a954032994106b856e8110564
xLyNT <50850168+xLyNT@users.noreply.github.com>
2019-05-30 19:12:17 +0300

    Create one.js

---
+++ one.js
@@ -0,0 +1,27 @@
+'use strict'
+function filter(arr, func){
+    var result = [];
+        for (var i = 0; i < arr.length; i++){
+            var n = arr[i];
+            if(func(n)){
+                result.push(n);
+            }
+         }
+         return result;
+}
+var arr = [1, 2, 3, 4, 5, 6, 7];
+function inBetween(a,b){
+    return function(x){
+        return x <= b && x >= a;
+    };
+}
+function inArray(arr){
+    return function (x){
+        return arr.indexOf(x) != -1;
+    };
+}
+alert(filter(arr, function(a) {
+  return a % 2 == 0
+}))
+alert(filter(arr,inBetween(2,4)));
+alert(filter(arr,inArray([3,5,7])));
